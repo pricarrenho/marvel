8999d8481f3c198b05467e4c604606b9e05615f3
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-31 16:13:57 -0300

    feat: add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes as RouteLib } from "react-router-dom";
+import { Hero } from "../pages/Hero";
 import { Home } from "../pages/Home";
 
 export const Routes = () => {
   return (
     <RouteLib>
       <Route path="/" element={<Home />} />
+      <Route path="/hero/:id" element={<Hero />} />
     </RouteLib>
   );
 };
