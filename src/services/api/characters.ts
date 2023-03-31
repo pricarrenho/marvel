236b55a21da7e03e0a64b037b7f32ddd7c62c275
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-31 16:12:41 -0300

    feat: characters and hero api's

---
+++ src/services/api/characters.ts
@@ -0,0 +1,25 @@
+import md5 from "md5";
+import axios from "axios";
+
+export const getMarvelCharacters = async (pagination: any) => {
+  const url = "http://gateway.marvel.com/v1/public/characters";
+  const timestamp = new Date().getTime();
+
+  const privateKey = "577b6115809153e62ecb5c567f7afc99da3f62a5";
+  const publicKey = "f4f576f97b7e03246e3e7b4df8c3b49d";
+
+  const hash = md5(`${timestamp}${privateKey}${publicKey}`);
+  const offset = Number(pagination) - 1;
+  const limit = 10;
+  const page = offset * limit;
+
+  return axios.get(url, {
+    params: {
+      apikey: publicKey,
+      ts: timestamp,
+      hash,
+      limit,
+      offset: page,
+    },
+  });
+};
---
+++ src/services/api/hero.ts
@@ -0,0 +1,20 @@
+import md5 from "md5";
+import axios from "axios";
+
+export const getMarvelHero = async (idHero: any) => {
+  const url = `http://gateway.marvel.com/v1/public/characters/${idHero}`;
+  const timestamp = new Date().getTime();
+
+  const privateKey = "577b6115809153e62ecb5c567f7afc99da3f62a5";
+  const publicKey = "f4f576f97b7e03246e3e7b4df8c3b49d";
+
+  const hash = md5(`${timestamp}${privateKey}${publicKey}`);
+
+  return axios.get(url, {
+    params: {
+      apikey: publicKey,
+      ts: timestamp,
+      hash,
+    },
+  });
+};
