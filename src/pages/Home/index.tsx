ce5f4b09d4f26bfb6e9e9a18a0fa778e09378b52
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-31 16:13:38 -0300

    feat: add page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import Footer from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Main } from "../../components/Main";
+
 export const Home = () => {
   return (
     <div>
-      <h1>Marvel</h1>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 };
