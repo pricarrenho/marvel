852dd20c03c88fbef591af96e041c2f575ca98b5
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-31 16:11:55 -0300

    style: global styles and themes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,14 +2,42 @@ import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   ${({ theme }) => css`
-    body {
-      margin: 0;
-      padding: 0;
-      background: ${theme.colors.background};
+    html {
+      font-size: 62.5%;
     }
 
-    h1 {
-      color: red;
+    body,
+    h1,
+    h2,
+    h3,
+    p {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    html,
+    body,
+    #__next {
+      background: ${theme.colors.background};
+      overflow: auto;
+      font-family: ${theme.font.family};
+    }
+
+    h2,
+    h3 {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      color: ${theme.colors.neutral[800]};
+    }
+
+    li {
+      list-style: none;
+    }
+
+    ul {
+      padding: 0;
     }
   `}
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,23 @@
 const colors = {
   black: "#13131A",
   white: "#FFFFFF",
+  red: "#FF0000",
+  neutral: {
+    50: "#fafafa",
+    100: "#f5f5f5",
+    200: "#e5e5e5",
+    300: "#d4d4d4",
+    400: "#a3a3a3",
+    500: "#737373",
+    600: "#525252",
+    700: "#404040",
+    800: "#262626",
+    900: "#13131a",
+  },
 };
 
 const font = {
+  family: "Roboto",
   sizes: {
     xsm: "1.4rem",
     sm: "1.6rem",
